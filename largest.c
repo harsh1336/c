d60f7aeb8619b932b03c789c2db9f8297b502097
harsh1336 <harshpandey2780@gmail.com>
2025-09-02 04:45:15 +0000

    largest.c

---
+++ largest.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,num3;
+    printf("enter three number");
+    scanf("%d%d%d",&num1,&num2,&num3);
+    if(num1>num2&&num1>num3)
+    {
+        printf("num1 is largest number ");
+
+    }else if(num2>num1&&num2>num3){
+        printf("num2 is largest number");
+
+    }else{ 
+        printf("num3 is largest number");
+    }
+
+    
+}
