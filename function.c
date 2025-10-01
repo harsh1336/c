aa036e4c4c45326fa75e57e9679a8febf43358e1
harsh1336 <harshpandey2780@gmail.com>
2025-10-01 04:33:01 +0000

    fun1.c

--- function.c
+++ function.c
@@ -7,31 +7,37 @@ void sub();
 
 int main(){
     // Function Call - Use
-// greet();
-// greet();
-// greet();
-// add(1,1);
-// add(2,2);
-// add(3,3);
-// add(31,32);
-// sub(1,3);
-// sub(1,7);
-// sub(1,9);
-}
+    // greet();
+    // greet();
+    // greet();
+    // add(1,1);
+    // add(2,2);
+    // add(3,3);
+    // add(31,32);
+    // sub(1,3);
+    // sub(1,7);
+    sub(10,12);//args - actual values
+    }
 
-// Function DEFINITION - Create
-void greet(){
-    printf("Good Morning. Mohini");
-}
+    // Function DEFINITION - Create
+    // void greet(){
+    //     printf("Good Morning. Mohini");
+    // }
 
 
 
-void add(int a,int b){
-   
-    printf("%d\n",a+b);
-}
-void sub(int a,int b){
-   
-    printf("%d\n",a-b);
-}
+    // void add(int a,int b){ //parameteres -placeholder
+       
+       //     printf("%d\n",a+b);
+       // }
+       // void sub(int a,int b){
+          
+          //     printf("%d\n",a-b);
+          // }
+
+          int get10(){
+              
+              }
+              
+
 
